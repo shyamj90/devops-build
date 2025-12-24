fdff9d860d463c5f559b935ebf47dcfe69d771b8
shyamj90 <ec2-user@ip-172-31-44-212.us-east-2.compute.internal>
2025-12-24 17:51:58 +0000

    minor

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,5 @@ set -e
 # Secure Transfer the manifests and configs to the App server
 scp -o StrictHostKeyChecking=no docker-compose.yaml prometheus.yml blackbox.yml ec2-user@172.31.35.56:/home/ec2-user/app/
 # SSH connection
-ssh -o StrictHostKeyChecking=no ec2-user@172.31.35.56 
+ssh -o StrictHostKeyChecking=no ec2-user@172.31.35.56 "cd /home/ec2-user/app && docker compose up -d"
 
-cd /home/ec2-user/app && docker compose up -d
