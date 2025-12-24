9b6cfa3191375d2329e12758b24c0351cc5fec59
shyamj90 <ec2-user@ip-172-31-44-212.us-east-2.compute.internal>
2025-12-24 16:35:35 +0000

    Added deploy script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,6 @@ set -e
 
 IMAGE_NAME="shyamj90"
 BRANCH_NAME=$1
-
+BRANCH_NAME="${BRANCH_NAME#origin/}"
 echo "Building Docker image for branch: $BRANCH_NAME"
 docker build -t ${IMAGE_NAME}/${BRANCH_NAME} .
